53fdf747ea054d6d38f5e6f7872c7ee0a3d278f1
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-11-11 19:35:34 -0500

    added json file and scripts to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
